2f0e5c13098d2f2e83ffde953e305a0b9a1bdc42
Lennart Reh <120135329+LMR271@users.noreply.github.com>
2026-06-30 17:05:44 +0200

    fininalized the Todo List with all the functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "tasks", to: "tasks#index", as: :tasklist
+  get "tasks/new", to: "tasks#new", as: :newtask
+  get "tasks/:id", to: "tasks#show", as: :details
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update", as: :update
+  post "tasks", to: "tasks#create", as: :createnew
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy
 end
